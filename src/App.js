e3cdb360c3edce1fb68abedb099f697f3685fea5
Daniel <dan_wong@outlook.com>
2018-04-13 20:54:06 +1200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        
+      </Router>
+    );
+  }
+}
+
+export default App;
